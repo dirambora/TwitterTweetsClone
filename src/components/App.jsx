2e4836d512a27c77555d2f13742d46d24c1220a1
unknown <dirambora>
2022-07-04 07:51:14 +0300

    Tweets CRUD

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,9 @@ import { nanoid } from 'nanoid'
 import ComposeForm from './ComposeForm'
 import Timeline from './Timeline'
 import { FaTwitter } from 'react-icons/fa'
+
+
+
 import './App.css'
 
 
@@ -14,8 +17,14 @@ const CURRENT_USER = 'Diram'
 const SOME_USER ='Deee'
 
 function App() {
+
+  
+ 
+   
   // We now store the tweets in the state of our component
   const [tweets, setTweets] = useState(initialTweets)
+  
+
 
   // This handler will be called when we want to post a new tweet**
   const handlePostTweet = (content) => {
@@ -34,14 +43,37 @@ function App() {
     setTweets([...tweets, newTweet])
   }
 
+  // const handleDelete = (id) => {
+  //   const delTodo = tweets.filter(id);
+  //   setTweets([...delTodo]);
+  // };
+
+  const handleDelete = (id) => {
+    const delTodo = tweets.filter((to) => to.id !== id);
+    setTweets([...delTodo]);
+  };
+
+
+ 
+
   return (
     <div className="app">
       <FaTwitter className="app-logo" size="2em" />
       <ComposeForm onSubmit={handlePostTweet} />
       <div className="separator"></div>
-      <Timeline tweets={tweets} />
+      <Timeline handleDelete={handleDelete} tweets={tweets} 
+     
+     
+      />
+      
+      
     </div>
   )
+
+  
+
+
+
 }
 
 export default App
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,33 +1,49 @@
 // Tweet.jsx
+
 import PropTypes from 'prop-types'
 import Avatar from './Avatar'
 import './Tweet.css'
-import { FaHeart } from 'react-icons/fa';
-import { BiMessageRounded } from "react-icons/bi";
-import { FaRetweet } from 'react-icons/fa';
+import initialTweets from '../tweets.json'
+import { useState } from 'react'
 
 function Tweet(props) {
-  const { username,user, createdOn, children,useractions } = props
+
+
+  const { username,user, createdOn, children } = props
 
   return (
+    
     <div className="tweet">
       <Avatar name={user} />
       <div>
         <div className="tweet-header">
-        <span className="tweet-user">{username}</span>
-          <span className="tweet-user">@{user}</span>
+        <span className="tweet-user">{username}
+        <span className="tweet-user">@{user}</span>
+        </span>
+         
           <span className="tweet-created-on">{createdOn}</span>
         </div>
         <div className="tweet-content">{children}</div>
-        <div className='action-buttons'>
-        <FaHeart className="app-logo" size="1.5em" />
-        <BiMessageRounded className="app-logo" size="1.5em" />
-        <FaRetweet className="app-logo" size="1.5em" />
-        </div>
+       
+      
+       
       </div>
     </div>
+
+    
   )
+
+  
+  
+
+  
 }
+
+
+
+
+ 
+
 
 Tweet.propTypes = {
   user: PropTypes.string,
